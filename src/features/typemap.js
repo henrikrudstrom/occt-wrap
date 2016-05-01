fc1058a97174243ff06c5fdf7dfe1d3599c1e874
henrikrudstrom <hrudstrom@gmail.com>
2016-05-01 16:11:02 +0200

    typemaps refactored

--- src/features/typemap.js
+++ src/features/typemap.js
@@ -2,75 +2,65 @@ const extend = require('extend');
 const features = require('../features');
 
 
-function typemap(native, wrapped, options) {
+function typemap(native, wrapped, renderer, options) {
   if (!this.typemaps)
     this.typemaps = [];
 
-  var map = { native, wrapped };
+  var map = { native, wrapped, renderer };
   extend(map, options);
-
   this.typemaps.push(map);
   features.registerTypemap(map);
 }
 
-function gpTypemap(native, wrapped, getter) {
-  return this.typemap(native, wrapped, {
-    getter,
-    render: true,
-    toNative: 'withAccessor',
-    toWrapped: 'withConstructor'
-  });
-}
-
-
-
-
 features.registerConfig(typemap);
-features.registerConfig(gpTypemap);
 
 
 // ----------------------------------------------------------------------------
 // Swig rendering
 // ----------------------------------------------------------------------------
 
-function withAccessor(tm) {
-  var message = `in method '" "$symname" "', argument " "$argnum"" of type '" "${tm.wrapped}""'`;
-  return (nativeObj, wrappedObj) =>
-    `void *argp ;
-  int res = SWIG_ConvertPtr(${wrappedObj}, &argp, SWIGTYPE_p_${tm.wrapped},  0 );
+function withAccessor(input, output, native, wrapped, getter) {
+  var message = `in method '" "$symname" "', argument " "$argnum"" of type '" "${wrapped}""'`;
+  return `void *argp ;
+  int res = SWIG_ConvertPtr(${input}, &argp, SWIGTYPE_p_${wrapped},  0 );
   if (!SWIG_IsOK(res)) {
     SWIG_exception_fail(SWIG_ArgError(res), "${message}");
   }
-  ${nativeObj} = (${tm.native} *)&((const ${tm.wrapped} *)(argp))->${tm.getter};`;
+  ${output} = (${native} *)&((const ${wrapped} *)(argp))->${getter};`;
 }
 
-function withConstructor(tm) {
-  return (nativeObj, wrappedObj) => {
-    var arg = `(new ${tm.wrapped}((const ${tm.native} &) ${nativeObj}))`;
-    var obj = `SWIG_NewPointerObj(${arg}, SWIGTYPE_p_${tm.wrapped}, SWIG_POINTER_OWN |  0);`;
-    return `${wrappedObj} = ${obj}`;
+function withConstructor(input, output, native, wrapped) {
+  var arg = `(new ${wrapped}((const ${native} &) ${input}))`;
+  var obj = `SWIG_NewPointerObj(${arg}, SWIGTYPE_p_${wrapped}, SWIG_POINTER_OWN |  0);`;
+  return `${output} = ${obj}`;
+}
+
+features.registerTypemapRenderer('member-object', function convertTypeRenderer(tm) {
+  return {
+    toNative(input, output) {
+      return withAccessor(input, output, tm.native, tm.wrapped, 'XYZ()');
+    },
+    toWrapped(input, output) {
+      return withConstructor(input, output, tm.native, tm.wrapped);
+    }
   };
-}
-
-
-
-features.registerNativeConverter(withAccessor);
-features.registerWrappedConverter(withConstructor);
-
+});
 
 function renderTypemap(tm) {
   var native = tm.native;
-  var convert = features.getTypemapConverter(tm.native);
+  var render = features.getTypemapRenderer(tm);
+
+  if (!render) return '';
 
   return `
 #include <${native}.hxx>
 %typemap(in) const ${native} &{
   // typemap inmap
-  ${convert.toNative('$1', '$input')}
+  ${render.toNative('$input', '$1')}
 }
 %typemap(out) ${native} {
   //typemap outmap
-  ${convert.toWrapped('$1', '$result')}
+  ${render.toWrapped('$1', '$result')}
 }
 `;
 }
@@ -80,13 +70,9 @@ function renderTypemaps(decl) {
   if (!decl.typemaps) return false;
 
   var src = decl.typemaps
-    .filter(tm => tm.render)
     .map(tm => renderTypemap(tm)).join('\n');
 
-  return [
-
-    { name: 'typemaps.i', src }
-  ];
+  return { name: 'typemaps.i', src };
 }
 
 features.registerRenderer('swig', 0, renderTypemaps);
